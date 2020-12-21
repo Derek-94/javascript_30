a89ccd469bc93969e8535169ca68cfb8100f1a9b
Derek-94 <ghdtjrrl94@gmail.com>
2020-12-21 20:46:54 +0900

    feat(main): update all function
    - color changes when draw in canvas.
    - linewidth keeps wide or thin as we draw

--- 08 - Fun with HTML5 Canvas/src/index.js
+++ 08 - Fun with HTML5 Canvas/src/index.js
@@ -9,13 +9,21 @@ ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 
+ctx.lineWidth = 50;
+
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
+let direction = true;
+
+let hue = 0;
+
 function draw(event){
     if(!isDrawing) return; // stop the function if it is not the drawing mode.
     console.log(event);
+
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
@@ -23,9 +31,28 @@ function draw(event){
     ctx.stroke();
     lastX = event.offsetX;
     lastY = event.offsetY;
+    
+    hue++;
+    if(hue >= 360){
+        hue = 0;
+    }
+
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+        direction = !direction;
+    }
+    if(direction){
+        ctx.lineWidth++;
+    }
+    else{
+        ctx.lineWidth--;
+    }
 }
 
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mousedown", (event) => {
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+});
 canvas.addEventListener("mouseup", () => isDrawing = false);
 canvas.addEventListener("mouseout", () => isDrawing = false); 
