61afb6fc3b98e461ed6532c1afbcef4063b5b061
Derek-94 <ghdtjrrl94@gmail.com>
2020-12-21 17:03:16 +0900

    feat(main): udpate canvas base func
    - update mouse function
    - initialize functions

---
+++ 08 - Fun with HTML5 Canvas/src/index.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("#draw");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(event){
+    if(!isDrawing) return; // stop the function if it is not the drawing mode.
+    console.log(event);
+
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false); 
