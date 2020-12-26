1ff2373d341e4db86c252f1527d535c61402a501
Derek-94 <ghdtjrrl94@gmail.com>
2020-12-26 18:39:53 +0900

    feat(main): add few funcs
    - adjusting volume.
    - can make video slow or fast.
    - clicking the play button and stop button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,3 +6,33 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+    video.paused ? video.play() : video.pause();
+}
+
+// The fact that this function is not included in "togglePlay()"
+// is meaningful. We listened to the "play" and "pause" event.
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.innerHTML = icon;
+}
+
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
+
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+skipButtons.forEach(btn => btn.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
